d6b10a3693c35d6ce398d860017fdf6e9dc3f761
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-02-17 01:40:36 +0600

    Dynamic function created for total expenses calculation

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,36 @@
 
+function getInputAmount(expensesSector) {
+    const SectorInput = document.getElementById(expensesSector + '-input');
+    const SectorInputText = SectorInput.value;
+    const SectorInputAmount = parseInt(SectorInputText);
+    return SectorInputAmount;
+}
+
+
 // handel calculate button event
 document.getElementById('calculate-button').addEventListener('click', function () {
 
     // find food expenses amount 
-    const foodInput = document.getElementById('food-input');
+    /* const foodInput = document.getElementById('food-input');
     const foodInputText = foodInput.value;
-    const foodInputAmount = parseInt(foodInputText);
+    const foodInputAmount = parseInt(foodInputText); */
+
+    const foodInputAmount = getInputAmount('food');
+
 
     // find rent expenses amount 
-    const rentInput = document.getElementById('rent-input');
-    const rentInputText = rentInput.value;
-    const rentInputAmount = parseInt(rentInputText);
+    /*  const rentInput = document.getElementById('rent-input');
+     const rentInputText = rentInput.value;
+     const rentInputAmount = parseInt(rentInputText); */
+
+    const rentInputAmount = getInputAmount('rent');
 
     // find clothes expenses amount 
-    const clothesInput = document.getElementById('clothes-input');
+    /* const clothesInput = document.getElementById('clothes-input');
     const clothesInputText = clothesInput.value;
-    const clothesInputAmount = parseInt(clothesInputText);
+    const clothesInputAmount = parseInt(clothesInputText); */
+
+    const clothesInputAmount = getInputAmount('clothes');
 
     //calculate total amount
     const calculateTotalAmount = foodInputAmount + rentInputAmount + clothesInputAmount;
