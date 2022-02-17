5f71bee504309b8902bd2df767a39a870c35446d
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-02-17 15:21:26 +0600

    bonus part with function completed

--- javascript/script.js
+++ javascript/script.js
@@ -6,37 +6,59 @@ function getInputAmount(expensesSector) {
     return SectorInputAmount;
 }
 
+function updateBalance(){
+
+    // find total income amount
+    const incomeInputAmount = getInputAmount('income');
+
+    // find food expenses amount 
+    const foodInputAmount = getInputAmount('food');
+
+    // find rent expenses amount 
+    const rentInputAmount = getInputAmount('rent');
+  
+    // find clothes expenses amount 
+    const clothesInputAmount = getInputAmount('clothes');
+  
+    //calculate total expenses amount
+    const  calculateTotalExpenses = foodInputAmount + rentInputAmount + clothesInputAmount;
+    
+    // update expenses total
+    const expensesTotal = document.getElementById('expenses-total');
+    expensesTotal.innerText = calculateTotalExpenses;
+    
+    // update balance after expenses total
+    const balance = incomeInputAmount - calculateTotalExpenses;
+
+    return balance;
+}
 
 // handel calculate button event
 document.getElementById('calculate-button').addEventListener('click', function () {
+     
+    // update balance after expenses total
+    const balance = updateBalance();
+    const balanceAfterExpenses = document.getElementById('balance_after_expenses');
+    balanceAfterExpenses.innerText = balance;
 
-    // find food expenses amount 
-    /* const foodInput = document.getElementById('food-input');
-    const foodInputText = foodInput.value;
-    const foodInputAmount = parseInt(foodInputText); */
-
-    const foodInputAmount = getInputAmount('food');
+});
 
 
-    // find rent expenses amount 
-    /*  const rentInput = document.getElementById('rent-input');
-     const rentInputText = rentInput.value;
-     const rentInputAmount = parseInt(rentInputText); */
+document.getElementById('save-button').addEventListener('click', function(){
 
-    const rentInputAmount = getInputAmount('rent');
+    // find income amount & save percentage 
+    const incomeInputAmount = getInputAmount('income');
+    const saveInputPercentage = getInputAmount('save');
 
-    // find clothes expenses amount 
-    /* const clothesInput = document.getElementById('clothes-input');
-    const clothesInputText = clothesInput.value;
-    const clothesInputAmount = parseInt(clothesInputText); */
+    // update save amount
+    const saveAmount = incomeInputAmount * (saveInputPercentage / 100);
+    const savingTotal = document.getElementById('saving-total');
+    savingTotal.innerText = saveAmount;
 
-    const clothesInputAmount = getInputAmount('clothes');
+    // update remaining amount 
+    const remainingBalance = updateBalance() - saveAmount;
+    const remainingTotal = document.getElementById('Remaining-balance');
+    remainingTotal.innerText = remainingBalance;
 
-    //calculate total amount
-    const calculateTotalAmount = foodInputAmount + rentInputAmount + clothesInputAmount;
-
-    // update expenses total
-    const expensesTotal = document.getElementById('expenses-total');
-    expensesTotal.innerText = calculateTotalAmount;
 
 })
